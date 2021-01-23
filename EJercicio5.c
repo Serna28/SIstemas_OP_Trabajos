3c2688515cac63ad20aea6b125409f833c09ccd8
Sebastian SErna <juan.sernacupb.edu.co>
2021-01-22 21:20:31 -0500

    Guia de EJercicios 5,Punto 2

--- EJercicio5.c
+++ EJercicio5.c
@@ -6,8 +6,23 @@ int main()
     //Pregunta 1
     printf( " ¿Qué es un lenguaje imperativo y qué otro lenguaje imperativo usted conoce muy bien? \n");
     printf(" Un lenguage imperativo son las instrucciones que se ejecutan unas tras otras, de manera secuencial, salvo cuando se encuentran estructuras de control condicionales o bucles \n");
-    printf(" Y Hasta el momento solo conozco C,C++,C#, Java y emsambladores por arquitectura ");
+    printf(" Y Hasta el momento solo conozco C,C++,C#, Java y emsambladores por arquitectura \n");
+    printf(" \n");
+
+    printf("------------------------------------ \n");
+
+
+    //pregunta 2
+
+    printf(" ¿Cuál es la diferencia con un lenguaje declarativo (no olvide el ejemplo)? \n");
+    printf("  los lenguajes imperativos proporcionan la receta, mientras que los declarativos, fotos del plato preparado \n");
+
+
+    printf(" -----------------------------------------  \n");
+
+
+   
     return 0;
 
-    
+
 }
