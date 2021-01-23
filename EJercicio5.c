a2075a08132c679854e43507fba7f67ca8982ff0
Sebastian SErna <juan.sernacupb.edu.co>
2021-01-22 21:07:01 -0500

    Guia de ejercicios 5

---
+++ EJercicio5.c
@@ -0,0 +1,13 @@
+#include <stdio.h> //Se debe importar, se me olvido y tuve un lapso mental
+
+
+int main()
+{
+    //Pregunta 1
+    printf( " ¿Qué es un lenguaje imperativo y qué otro lenguaje imperativo usted conoce muy bien? \n");
+    printf(" Un lenguage imperativo son las instrucciones que se ejecutan unas tras otras, de manera secuencial, salvo cuando se encuentran estructuras de control condicionales o bucles \n");
+    printf(" Y Hasta el momento solo conozco C,C++,C#, Java y emsambladores por arquitectura ");
+    return 0;
+
+    
+}
