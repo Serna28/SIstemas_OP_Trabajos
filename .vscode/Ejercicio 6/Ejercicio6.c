44aa2e91d4cf1a575c685c1d6e57b8b99d122aa0
Sebastian SErna <juan.sernacupb.edu.co>
2021-01-27 14:33:06 -0500

    EJercicio 6, Punto 1 al 6

---
+++ .vscode/Ejercicio 6/Ejercicio6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    //EJercicio 1
+
+    int var1 = 11;
+    int var2 = 12;
+    int *pvar1;
+    pvar1 = &var1; 
+
+    //Obtener  //Almacenar
+    int *pvar2 = &var2;
+
+
+    //EJercicio 2
+    int *pvar;
+    int pvar2;
+
+    pvar1 = pvar2;
+
+    //EJErcicio 3
+}
