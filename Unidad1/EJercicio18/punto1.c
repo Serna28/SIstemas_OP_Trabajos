9fc1f358108ddc6171aa6e2eb4e7b8e9a9f6e5af
Serna <juan.sernacupb.edu.co>
2021-02-07 11:33:34 -0500

    EJercicio18 FUll

---
+++ Unidad1/EJercicio18/punto1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+ #define MAX 100
+
+ void imprimir(int *pelementos,int a)
+ {
+
+     printf("\n El vector es : \n");
+
+     for(int i = 0; i < a ;i++) 
+     {
+         printf("Elemto[%d]: %d\n",i,  *(pelementos+i) );
+     }
+ }
+
+ int main(){
+     int a;
+     int elementos[MAX];
+     int cambio;
+
+     printf("Ingrese el tamaño del vector: \n");
+     scanf("%d", &a);
+
+
+     for(int i = 0; i < a; i++)
+     {
+         printf("El elemento en la poscicion [%i] es : ",i);
+         scanf("%d", &elementos[i]);
+     }
+     imprimir(elementos, a);
+
+     printf("\n Ingrese que posicion desea cambiar: \n");
+     scanf("%d", &cambio);
+     cambio--;
+     for(int i = a;i >= cambio; i--)
+     {
+         elementos[i+1] = elementos[i];
+     }
+     printf("Ingrese el nuevo valor del vector : ");
+     scanf("%d", &elementos[cambio]);
+
+     imprimir(elementos,a);
+     return 0;
+ }
